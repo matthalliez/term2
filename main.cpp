fde5a6c174d1c66a0f26de547093b6820dbf255f
matthalliez <127944507+matthalliez@users.noreply.github.com>
2025-04-11 13:51:54 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 void coreFunctions(){
     HashMap<std::string, int> testMap;
+    int value;
 
     //demostrating the insert method
     std::cout << "insert(): " << std::endl;
@@ -16,21 +17,33 @@ void coreFunctions(){
 
     //demonstarting the remove method
     std::cout << "remove(): " << std::endl;
-    std::cout << "Test 2.1: removing 'Calvin': " << (testMap.remove("Calvin") "Passed" : "Failed") << std::endl;
+    std::cout << "Test 2.1: removing 'Calvin': " << (testMap.remove("Calvin") ? "Passed" : "Failed") << std::endl;
 
     //demonstrating the get method
     std::cout << "get(): " <<std::endl;
-    std::cout << "Test 3.1: getting value for 'Matt': " << (testMap.get("Matt", value) ? value : "Failed") << std::endl;
-    std::cout << "Test 3.2: getting value for 'Calvin': " << (testMap.get("Calvin", value) ? value : "Failed") << std::endl;
+    std::cout << "Test 3.1: getting value for 'Matt': ";
+    if(testMap.get("Matt", value)){
+        std::cout <<value << std::endl;
+    } else{
+        std::cout << "Failed" <<std::endl;
+    }
+    std::cout << "Test 3.2: getting value for 'Calvin': ";
+    if(testMap.get("Calvin", value)){
+        std::cout <<value << std::endl;
+    } else{
+        std::cout << "Failed" <<std::endl;
+    }
 }
 //clock helper method
-double clock(Func&& func){
+template <typename Func>
+double clocker(Func&& func){
     auto start = std::chrono::high_resolution_clock::now();
     func();
     auto end = std::chrono::high_resolution_clock::now();
     return std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() / 1000.0;
 }
 void performanceTime(){
+    int value;
     //random number generator
     std::random_device rd; 
     std::mt19937 gen(rd()); 
@@ -46,21 +59,21 @@ void performanceTime(){
             keys[i] = dis(gen);
         }
         //measuring the insert time
-        double insertTime = clock([&](){
+        double insertTime = clocker([&](){
             for(int i =0; i<size; i++){
                 testMap.insert(keys[i],i);
             }
         });
         std::cout << "Insert time: " << insertTime << std::endl;
         //measuring the remove time
-        double removetTime = clock([&](){
+        double removeTime = clocker([&](){
             for(int i =0; i<size; i++){
                 testMap.remove(keys[i]);
             }
         });
         std::cout << "Remove time: " << removeTime << std::endl;
         //measuring the get time
-        double getTime = clock([&](){
+        double getTime = clocker([&](){
             for(int i =0; i<size; i++){
                 testMap.get(keys[i],value);
             }
